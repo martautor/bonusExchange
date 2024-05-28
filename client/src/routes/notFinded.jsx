7bd10b4ca3feb089ab06672e3f620870e0617447
martautor <167963978+martautor@users.noreply.github.com>
2024-05-28 11:23:48 +0300

    function notfinded changes

--- client/src/routes/notFinded.jsx
+++ client/src/routes/notFinded.jsx
@@ -10,19 +10,9 @@ import { brown } from '@mui/material/colors'
 
 export default function notFinded() {
     return (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center', margin: 0, padding: 0}}>
-        <h2>Похоже ваш номер телефона не зарегистрирован в системе лояльности BonusMoney.</h2>
-        <h2>Скачайте приложение на телефон, и зарегистрируйтесь:</h2>
-        <div style={{display: 'flex'}}>
-            <div style={{display: 'grid', justifyItems: 'center'}}>
-                <img src={qrcodeGP} style={{width: 180, height: 180}} alt="google_qr-code" />
-                <a href="https://play.google.com/store/apps/details?id=ru.mobint.pivmarket1&pcampaignid=web_share" target="_blank" rel="noreferrer"><img src={google} style={{width: 150, height: 49}} alt='google'/></a>
-            </div>
-            <div style={{display: 'grid', justifyItems: 'center'}}>
-                <img src={qrcodeAS} alt='appstore_qr-code'/>
-                <a href="https://apps.apple.com/ru/app/%D0%BF%D0%B8%D0%B2%D0%BC%D0%B0%D1%80%D0%BA%D0%B5%D1%82-1/id6473837140" target="_blank" rel="noreferrer"><img src={appstore} style={{width: 150, height: 50}} alt='appstore'/></a>
-            </div>
-        </div>
+        <h3>Похоже ваш номер телефона не зарегистрирован в системе лояльности BonusMoney.</h3>
+        <h4>Скачайте приложение на телефон, и зарегистрируйтесь:</h4>
+        <a href='https://pm26.ru' style={{ marginTop: 25}}><Button size='large' style={{backgroundColor:brown[800]}} variant="contained" color='success'> Как скачать? </Button></a>
         <Link to='/' style={{ marginTop: 50}}><Button size='large' style={{backgroundColor:brown[400]}} variant='contained'> Назад </Button></Link>
-        <a href='https://pm26.ru' style={{ marginTop: 25}}><Button size='large' style={{backgroundColor:brown[800]}} variant="contained" color='success'> На главную </Button></a>
     </div>)
 }
